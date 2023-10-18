31b05c2cb1948c96327fa39ace060601f7640320
apiyo <franklineapiyo@gmail.com>
2023-10-18 11:41:35 +0300

    Update IP addresses to and vagrant configs for example deployment

--- examples/load-balanced/vagrant/hosts/API1.rb
+++ examples/load-balanced/vagrant/hosts/API1.rb
@@ -2,10 +2,10 @@ Vagrant.configure("2") do |config|
   config.vm.define "api1" do |api1|
     api1.vm.provider :virtualbox do |vb|
       vb.name = "ansible-onadata-lb-api1"
-      vb.memory = 4096
-      vb.cpus = 2
+      vb.memory = 1024
+      vb.cpus = 1
     end
 
-    api1.vm.network "private_network", ip: "10.0.0.4"
+    api1.vm.network "private_network", ip: "192.168.56.4"
   end
 end
--- examples/load-balanced/vagrant/hosts/Samba.rb
+++ examples/load-balanced/vagrant/hosts/Samba.rb
@@ -2,10 +2,10 @@ Vagrant.configure("2") do |config|
   config.vm.define "samba" do |samba|
     samba.vm.provider :virtualbox do |vb|
       vb.name = "ansible-onadata-lb-samba"
-      vb.memory = 512
+      vb.memory = 1024
       vb.cpus = 1
     end
 
-    samba.vm.network "private_network", ip: "10.0.0.6"
+    samba.vm.network "private_network", ip: "192.168.56.6"
   end
 end
